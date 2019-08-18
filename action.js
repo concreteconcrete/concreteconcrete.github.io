6a4bdfcb97083b2018a9428872d53f7f7a3a10dc
Andi Wang <andywcmu@gmail.com>
2019-08-18 14:54:36 -0400

    add dialog

---
+++ action.js
@@ -0,0 +1,9 @@
+function on_load() {
+  const dialog = document.getElementById('dialog-full-container');
+  dialog.style.opacity = 1.0;
+}
+
+function close_dialog() {
+  const dialog = document.getElementById('dialog-full-container');
+  dialog.style.opacity = 0.0;
+}
